e10653a3a5e135a81aeb10d543d2781bcfa1b7d1
Nicholas Reyaad Rafeek <45085276+Reyaad510@users.noreply.github.com>
2024-03-04 20:39:12 -0500

    Initialization

---
+++ app.js
@@ -0,0 +1,13 @@
+// function createInstructor(firstName, lastName) {
+//   return {
+//     firstName: firstName,
+//     lastName: lastName,
+//   };
+// }
+
+function createInstructor(firstName, lastName) {
+  return {
+    firstName,
+    lastName,
+  };
+}
