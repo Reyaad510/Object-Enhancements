aec242a7ec83509c9a2c0a9899e4d5913f8f8859
Nicholas Reyaad Rafeek <45085276+Reyaad510@users.noreply.github.com>
2024-03-04 20:54:44 -0500

    Project completed

--- app.js
+++ app.js
@@ -11,3 +11,36 @@ function createInstructor(firstName, lastName) {
     lastName,
   };
 }
+
+// const favoriteNumber = 42;
+// const instructor = {
+//   firstName: "Colt",
+//   [favoriteNumber]: "That is my favorite!",
+// };
+
+const instructor = {
+  firstName: "Colt",
+  sayHi() {
+    return "Hi!";
+  },
+  sayBye() {
+    return `${this.firstName} says bye!`;
+  },
+};
+
+const d = createAnimal("dog", "bark", "Woooof!");
+// {species: "dog", bark: ƒ}
+d.bark(); //"Woooof!"
+
+const s = createAnimal("sheep", "bleet", "BAAAAaaaa");
+// {species: "sheep", bleet: ƒ}
+s.bleet(); //"BAAAAaaaa"
+
+function createAnimal(species, verb, noise) {
+  return {
+    species,
+    [verb]() {
+      return noise;
+    },
+  };
+}
